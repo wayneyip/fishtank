3add1010ed079aa2fc64b34cf165e0c676ccec59
Wayne Yip <wqyip97@gmail.com>
2024-07-03 21:53:23 +0800

    Spawn cones randomly on screen

--- main.js
+++ main.js
@@ -5,12 +5,41 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Lighting
+const dirLight = new THREE.DirectionalLight()
+scene.add(dirLight)
+const ambLight = new THREE.AmbientLight()
+scene.add(ambLight)
 
 // Geometry
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.ConeGeometry(1, 2)
+const material = new THREE.MeshLambertMaterial({ color: 0xaaaaaa })
+var boids = []
+const boidScale = 0.1;
+const spawnRange = 3;
+const boidCount = 100;
+const boidVelocity = new THREE.Vector3(1, 1, 1);
+
+function randomNumber(min, max) 
+{
+	return Math.random() * (max - min) + min;
+}
+
+for (let i=0; i < boidCount; i++)
+{
+	const boid = new THREE.Mesh(geometry, material)	
+	boid.scale.x = boidScale;
+	boid.scale.y = boidScale;
+	boid.scale.z = boidScale;
+	const x = randomNumber(-spawnRange, spawnRange)
+	const y = randomNumber(-spawnRange, spawnRange)
+	const z = randomNumber(-spawnRange, spawnRange)
+	boid.position.x = x; 
+	boid.position.y = y; 
+	boid.position.z = z; 
+	scene.add(boid)
+	boids.push(boid)
+}
+dirLight.target = boids[25]
 
 // Screen size
 const size = {
@@ -47,7 +76,9 @@ const tick = () => {
 	const elapsedTime = clock.getElapsedTime()
 
 	// Update objects
-	mesh.position.y = Math.sin(elapsedTime)
+	for (let boid of boids)
+	{
+	}
 
 	// Render
 	renderer.render(scene, camera)
