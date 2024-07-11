c8af29651bf2b72a5b696d6ba5df03293632c3d9
Wayne Yip <wqyip97@gmail.com>
2024-07-11 14:02:36 +0800

    Add lil-gui to project

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import * as THREE from 'three'
 import {BoidGroup} from './BoidGroup'
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js'
 import {DRACOLoader} from 'three/addons/loaders/DRACOLoader.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'lil-gui'
+
 import fishVertexShader from './shaders/fishVertex.glsl'
 import fishFragmentShader from './shaders/fishFragment.glsl'
 import waterVertexShader from './shaders/waterVertex.glsl'
 import waterFragmentShader from './shaders/waterFragment.glsl'
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
 const canvas = document.querySelector('canvas.webgl')
+
+// GUI
+const gui = new GUI()
 
 // Scene
 const scene = new THREE.Scene()
