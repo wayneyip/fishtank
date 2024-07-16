dad9d3a374229e6fdc8a112420af50239dd1336b
Wayne Yip <wqyip97@gmail.com>
2024-07-16 23:10:10 +0800

    Add vertical fade to godray alpha

--- src/shaders/godraysFragment.glsl
+++ src/shaders/godraysFragment.glsl
@@ -8,9 +8,11 @@ uniform float uTime;
 uniform float uSpeed;
 
 varying vec2 vUv;
+varying vec3 vPos;
 
 void main()
 {
+	// Polar coordinate texture sampling
 	vec2 center = vec2(0.5, 0.5);
 
 	vec2 delta = vUv - center;
@@ -20,8 +22,13 @@ void main()
 	vec2 polarUv = vec2(radius, angle);
 	polarUv.x = polarUv.x + uTime * uSpeed;
 
-	vec4 noise = texture2D(uMap, polarUv);
-	float alpha = dot(noise.xyz, vec3(0.3, 0.59, 0.11));
+	vec4 radialNoise = texture2D(uMap, polarUv);
+	float radialAlpha = dot(radialNoise.xyz, vec3(0.3, 0.59, 0.11)); // brightness
+
+	// Vertical fade
+	float verticalFade = mix(1.0, 0.0, vPos.z);
+
+	float alpha = radialAlpha * verticalFade;
 
 	gl_FragColor = vec4(uTint.xyz, alpha);
 }
--- src/shaders/godraysVertex.glsl
+++ src/shaders/godraysVertex.glsl
@@ -1,8 +1,10 @@
 varying vec2 vUv;
+varying vec3 vPos;
 
 void main()
 {
 	vUv = uv; 
+	vPos = position;
 	
 	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
